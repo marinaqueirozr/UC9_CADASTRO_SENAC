863e5c07dd42e96ab53c84756a3578bc3e5547b1
marinaqueirozr <marinaqueiroz1506@gmail.com>
2023-03-07 22:14:56 -0300

    Em processo

--- UC9_CADASTRO_SENAC/Program.cs
+++ UC9_CADASTRO_SENAC/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿string [,] ucs = new string [2, 3];
+string pesquisar = "";
+int l, c;
+double numero, cargah;
+
+l = 0;
+c = 0;
+numero = 0;
+cargah = 0;
+
+for (l = 0; l <= 1; l++)
+{
+    Console.WriteLine("Informe NÚMERO da UC: ");
+    ucs[l, c++] = Console.ReadLine();
+    Console.WriteLine("DESCRIÇÃO da UC: ");
+    ucs[l, c++] = Console.ReadLine();
+    Console.WriteLine("Informe a CARGA HORÁRIA: ");
+    ucs[l, c++] = Console.ReadLine();
+
+    c = 0;
+}
+
+
+
+switch (pesquisar)
+{
+    case 1:
+        for (l = 0; l <= 1; l++)
+        {
+            Console.WriteLine("Informe a UC que deseja pesquisar:");
+            pesquisar = Console.ReadLine();
+            if (pesquisar == ucs)
+            {
+
+            }
+        }
+        break;
+
+}
+
+
+
